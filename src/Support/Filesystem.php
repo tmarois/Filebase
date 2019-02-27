be06dfdcb169d32eb8a6ee81e5a87f4b8b9a8c8c
Timothy Marois <timothymarois@gmail.com>
2019-02-27 18:58:58 -0500

    Updated composer and a filesystem wrapper class

---
+++ src/Support/Filesystem.php
@@ -0,0 +1,14 @@
+<?php namespace Filebase\Support;
+
+/**
+ * The filesystem controls the files
+ * and diretories of Filebase
+ * 
+ * Filesystem uses 
+ * @see https://flysystem.thephpleague.com/docs/
+ * 
+ */
+class Filesystem
+{
+
+}
