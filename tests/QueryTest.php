3519443420f70a341679b3afc572160795924da6
faryar76 <faryar76@hotmail.com>
2019-03-01 00:32:50 +0330

    test

--- tests/QueryTest.php
+++ tests/QueryTest.php
@@ -34,5 +34,37 @@ class QueryTest extends TestCase
         $db=$this->query->getDatabase();
         $this->assertInstanceOf(Database::class,$db);
     }
-   
+    /**
+     * @test
+     */
+    public function testMustInsertNewRecordOnTable()
+    {
+
+        // check create
+        $this->query->create(['name'=>'John','last_name'=>'Doe']);
+        $this->assertFileExists($this->path."/tbl_one/0.json");
+
+        $this->query->create(['name'=>'John','last_name'=>'Doe']);
+        $this->assertFileExists($this->path."/tbl_one/1.json");
+
+        // check file have data
+        $this->assertJsonStringEqualsJsonFile(
+            $this->path."/tbl_one/1.json", json_encode(['name'=>'John','last_name'=>'Doe'])
+        );
+    }
+    /** @test */
+    public function testMustReturnDbRecordWithId()
+    {
+        $this->query->create(['name'=>'John','last_name'=>'Doe']);
+        $record=$this->query->find(0);
+
+        $this->assertEquals(['name'=>'John','last_name'=>'Doe'],$record);
+    }
+    /** @test */
+    public function testMustReturnInstanceOfCollection()
+    {
+        // Test
+    }
+    
+    
 }
