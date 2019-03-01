645bd30d6d90f36f813779d626fe0c95fc082ccf
faryar76 <faryar76@hotmail.com>
2019-03-01 14:21:54 +0330

    add local db for test cases

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,21 +2,30 @@
 namespace Filebase\Test;
 
 use PHPUnit\Framework\TestCase as base_case;
+use Filebase\Database;
 
 class TestCase extends base_case
 {
     public $path=__DIR__.'/tmp/';
+    public $tmp_db;
 
     protected function setUp():void
     {
         @mkdir($this->path);
-    }
+        $this->tmp_db=new Database([
+          'path'=>$this->path
+        ]);
 
+    }
+    
     protected function tearDown():void
     {
         $this->rrmdir($this->path);
     }
 
+    /**
+     * remove folder with sub items after each test 
+     */
     public function rrmdir($dir) 
     { 
         if (is_dir($dir)) { 
