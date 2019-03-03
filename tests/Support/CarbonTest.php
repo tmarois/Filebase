2905bbccf4c1c72ee698434892a2e1efdc4fe343
faryar76 <faryar76@hotmail.com>
2019-03-03 21:43:36 +0330

    test

---
+++ tests/Support/CarbonTest.php
@@ -0,0 +1,17 @@
+<?php 
+
+use Filebase\Test\TestCase;
+
+/**
+ * ClassNameTest
+ * @group group
+ */
+class ClassNameTest extends TestCase
+{
+    /** @test */
+    public function test_function()
+    {
+        // Test
+    }
+
+}
